0f5b005396b4bcb93553cdc3b39797d0a9dde0dc
Ed Martin <edman007@edman007.com>
2020-08-09 19:07:42 -0400

    Slackware Packaging Script Works chiton-install script works, sets up the directories and sets permissions Fixed Makefile to get the right git version when packaging Updated config files to work in a system install configuration TODO:create tables from the backend daemon

--- web/inc/config.template.php
+++ web/inc/config.template.php
@@ -22,10 +22,10 @@
  */
 
 
-define('DB_HOST', '@DB_HOST@');
-define('DB_DB', '@DB_DB@');
-define('DB_USER', '@DB_USER@');
-define('DB_PASS', '@DB_PASS@');
+define('DB_HOST', @DB_HOST@);
+define('DB_DB', @DB_DB@);
+define('DB_USER', @DB_USER@);
+define('DB_PASS', @DB_PASS@);
 define('SMARTY_COMPILE_DIR', '@SMARTY_COMPILE_DIR@');
 define('SMARTY_CACHE_DIR', '@SMARTY_CACHE_DIR@');
 ?>
