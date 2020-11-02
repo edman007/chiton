db64155d8674566ebd6eefc81b0583814190dfef
Ed Martin <edman007@edman007.com>
2020-11-02 16:08:06 -0500

    Lower the severity of a default config to DEBUG

--- config.cpp
+++ config.cpp
@@ -124,7 +124,7 @@ double Config::get_value_double(const std::string& key){
 const std::string& Config::get_default_value(const std::string& key){
     for (auto& itr : setting_options){
         if (itr.key == key){\
-            LWARN("Got default value of " + itr.def);
+            LDEBUG("Got default value of " + itr.def);
             return itr.def;
         }
     }
