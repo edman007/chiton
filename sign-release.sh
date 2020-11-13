835cce04cc1dd09b6102c7e23a91e07818f4344b
Ed Martin <edman007@edman007.com>
2020-11-13 16:41:37 -0500

    Bump Version Update Release Signing Script

--- sign-release.sh
+++ sign-release.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd release
-gpg --default-key chiton@edman007.com --clearsign *.dsc
-gpg-sign --armor --default-key chiton@edman007.com *
+gpg --armor --default-key chiton@edman007.com --clearsign *.dsc
+mv *.dsc.asc *.dsc
+for i in *.[xg]z ; do
+	gpg-sign  --default-key  chiton@edman007.com  -b  $i
+done
