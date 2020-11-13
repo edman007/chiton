a3aeaa896e659b4de076f196811c16332319698e
Ed Martin <edman007@edman007.com>
2020-11-13 15:38:12 -0500

    Switch to latest breeze-icons commit

--- update-external.sh
+++ update-external.sh
@@ -3,6 +3,9 @@
 #this goes through all external libs and updates them to the latest tag
 cd external
 for i in * ; do
+    if [ "$i" == "npm" ]; then
+        continue;
+    fi
     cd $i
     echo $i
     git checkout master
