36944b765ac9df285d9d98f3c447aad1839874ce
Ed Martin <edman007@edman007.com>
2020-11-13 15:56:02 -0500

    Delete unused script

--- build-svg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-convert -background none -density 288 web/static/bottom-cursor.svg  web/static/4x-bottom-cursor.png &
-convert -background none -density 144 web/static/bottom-cursor.svg  web/static/2x-bottom-cursor.png
