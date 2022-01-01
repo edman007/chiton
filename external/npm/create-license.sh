d69382c7af6ed93cab5f582a85e4a0f27883c224
Ed Martin <edman007@edman007.com>
2022-01-01 18:06:38 -0500

    Fix NPM deps  - eventemitter is not a dependency  - Fix the npm scripts to include all licenses when parsing    (even if no newline at EOF)

--- external/npm/create-license.sh
+++ external/npm/create-license.sh
@@ -9,7 +9,7 @@ fi
 rm debian.license.in LICENSE.* || true
 #make this a temp file so we don't have race conditions if it's run twice concurrently
 TMP_TARGET=$(mktemp debian.license.in.XXXXXX )
-while read line; do
+while read line || [ -n "$line" ]; do
     name=$(echo "$line" | cut -d , -f 2 | sed -e 's/^"//' -e 's/"$//')
     version=$(echo "$line" | cut -d , -f 3 | sed -e 's/^"//' -e 's/"$//' )
     licenses=$(echo "$line" | cut -d , -f 4 | sed -e 's/^"//' -e 's/"$//')
